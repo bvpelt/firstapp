4d738a8a858b3e88ce97d40368cb7a41c7759ad8
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2022-03-22 21:24:26 +0100

    defer, panic, recover

--- Main.go
+++ Main.go
@@ -20,6 +20,58 @@ var (
 )
 
 func main() {
+	// defer is called before panic
+	fmt.Println("start")
+	panicker()
+	panic("something bad happened")
+	fmt.Println("end")
+}
+
+func panicker() {
+	fmt.Println("about to panic")
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Error:", err)
+			panic(err)
+		}
+	}()
+}
+
+func test04() {
+	// defer is called before panic
+	fmt.Println("start")
+	defer fmt.Println("this was deferred")
+	panic("something bad happened")
+	fmt.Println("end")
+}
+
+func test03() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello Go!"))
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("err.Error()")
+	}
+}
+
+func test02() {
+	var myURL = "https://www.google.com/robots.txt"
+	res, err := http.Get(myURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	robots, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", robots)
+}
+
+func test01() {
 	args := os.Args
 	fmt.Printf("All arguments: %v\n", args)
 	argsWithoutProgram := os.Args[1:]
@@ -31,13 +83,15 @@ func main() {
 	fmt.Printf("counter: %v\n", counter)
 	fmt.Printf("actor %v\n", actorName)
 
-	var myURL = "https://nu.nl"
-	if len(argsWithoutProgram) == 1 {
-		myURL = argsWithoutProgram[0]
-	}
+	/*
 
-	// get web page
-	getWebPage(myURL)
+		if len(argsWithoutProgram) == 1 {
+			myURL = argsWithoutProgram[0]
+		}
+
+		// get web page
+		getWebPage(myURL)
+	*/
 }
 
 func getWebPage(page string) {
@@ -46,7 +100,6 @@ func getWebPage(page string) {
 	resp, err := http.Get(page)
 
 	if err != nil {
-
 		log.Fatal(err)
 	}
 
@@ -54,7 +107,6 @@ func getWebPage(page string) {
 	fmt.Println(resp.StatusCode)
 	fmt.Printf("\n=========== Headers  =========\n")
 	for k, v := range resp.Header {
-
 		fmt.Printf("%s %s\n", k, v)
 	}
 	fmt.Printf("\n=========== Headers  =========\n")
