a1e4fbbc8472cdaddf5e76c53e3489ff1ea2b931
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2022-04-05 20:27:29 +0200

    Functions

--- Main.go
+++ Main.go
@@ -20,6 +20,140 @@ var (
 )
 
 func main() {
+	g := greeter{
+		greeting: "Hello",
+		name:     "Go",
+	}
+	g.greet()
+	fmt.Println("The new name is: ", g.name)
+
+	g.greets()
+	fmt.Println("The new name is: ", g.name)
+}
+
+type greeter struct {
+	greeting string
+	name     string
+}
+
+func (g *greeter) greets() { // passing context g greeter by reference
+	fmt.Println(g.greeting, g.name)
+	g.name = "set by greets"
+}
+func (g greeter) greet() { // passing context g greeter by value
+	fmt.Println(g.greeting, g.name)
+	g.name = ""
+}
+
+func test13() {
+	var f func(msg string) = func(msg string) {
+		fmt.Println(msg)
+	}
+	f("Hello Go!")
+
+	var d func(a, b float64) (float64, error) = divide
+	x, err := d(5.0, 10.0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(x)
+}
+
+func test12() { // error handling
+	d, err := divide(5.0, 10.0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(d)
+}
+
+func divide(a, b float64) (float64, error) {
+	if b == 0.0 {
+		return 0.0, fmt.Errorf("Cannot divide by zero")
+	}
+	return a / b, nil
+}
+
+func test11() {
+	s := sum(1, 2, 3, 4, 5)
+	fmt.Println("The sum is: ", s)
+}
+
+func sum(values ...int) int {
+	fmt.Println("values: ", values)
+	result := 0
+
+	for _, v := range values {
+		result += v
+	}
+
+	fmt.Println("The sum is: ", result)
+
+	return result
+}
+
+func test10() {
+	for i := 0; i < 5; i++ {
+		sayMessage("Hello Go!", "yes", i)
+	}
+}
+
+func sayMessage(msg, msg1 string, idx int) {
+	fmt.Println(msg, msg1)
+	fmt.Println("The value of the index is: ", idx)
+}
+
+func test09() {
+	a := map[string]string{"foo": "bar", "baz": "buz"}
+	b := a // b points to address of map a!
+	fmt.Println(a, b)
+	a["foo"] = "qux"
+	fmt.Println(a, b)
+}
+
+func test08() {
+	var ms *myStruct
+	fmt.Println(ms)
+	ms = &myStruct{foo: 42}
+	fmt.Println(ms)
+	ms = new(myStruct)
+	(*ms).foo = 48
+	fmt.Println(ms)
+	ms.foo = 52
+	fmt.Println(ms, ms.foo)
+
+	a := []int{1, 2, 3} // if array has size b is copy if array has no size (is a slice) b has address
+	b := a
+	fmt.Println(a, b)
+	a[1] = 42
+	fmt.Println(a, b)
+}
+
+type myStruct struct {
+	foo int
+}
+
+func test07() {
+	a := [3]int{1, 2, 3}
+	b := &a[0]
+	c := &a[1]
+	fmt.Printf("%v %p %p\n", a, b, c)
+}
+
+func test06() { // Pointers
+	var a int = 42
+	var b *int = &a
+	fmt.Println(&a, b)
+	fmt.Println(a, *b)
+	a = 27
+	fmt.Println(a, *b)
+	*b = 31
+	fmt.Println(a, *b)
+}
+
+func test05() {
 	// defer is called before panic
 	fmt.Println("start")
 	panicker()
